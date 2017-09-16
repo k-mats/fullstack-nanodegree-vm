b3225f9be221ee69fdfc26804f337d50a8ac770f
k-mats <gfeenhaftsylphid@gmail.com>
2017-09-16 21:12:42 +0900

    Add database_setup.py only with Category and Item
    User table will be added later

---
+++ vagrant/catalog/database_setup.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+
+class Item(Base):
+    __tablename__ = 'item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    caregory = relationship(Category)
+
+
+engine = create_engine('sqlite:///catalog.db')
+
+
+Base.metadata.create_all(engine)
