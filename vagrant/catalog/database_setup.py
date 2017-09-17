292fa69ada545dd620b7d36adb50161bde4cc2ef
k-mats <gfeenhaftsylphid@gmail.com>
2017-09-17 14:39:14 +0900

    Add serialize method to model classes

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -12,16 +12,31 @@ class Category(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
 
 class Item(Base):
     __tablename__ = 'item'
 
-    name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
     description = Column(String(250))
     category_id = Column(Integer, ForeignKey('category.id'))
     caregory = relationship(Category)
 
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'category_id': self.category_id
+        }
 
 engine = create_engine('sqlite:///catalog.db')
 
