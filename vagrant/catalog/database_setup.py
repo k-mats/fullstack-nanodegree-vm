a00e2d55bb751f7e4e02706cf114d75f10bcdaf3
k-mats <gfeenhaftsylphid@gmail.com>
2017-09-18 14:48:31 +0900

    Implement log-in feature by Google OAuth

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -6,6 +6,15 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
 class Category(Base):
     __tablename__ = 'category'
 
@@ -25,7 +34,7 @@ class Item(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(80), nullable=False)
-    description = Column(String(250))
+    description = Column(String(250), nullable=False)
     category_id = Column(Integer, ForeignKey('category.id'))
     caregory = relationship(Category)
 
