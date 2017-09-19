8b2532130bf6bca4050c02dc59179516b9896920
k-mats <gfeenhaftsylphid@gmail.com>
2017-09-19 16:25:53 +0900

    Make API secure by OAuth and token-based verification

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -2,8 +2,11 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+import random, string
+from itsdangerous import(TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
 
 Base = declarative_base()
+secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
 
 
 class User(Base):
@@ -11,8 +14,26 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    email = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False, index=True)
     picture = Column(String(250))
+
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(secret_key, expires_in = expiration)
+        return s.dumps({'id': self.id })
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(secret_key)
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            #Valid Token, but expired
+            return None
+        except BadSignature:
+            #Invalid Token
+            return None
+            user_id = data['id']
+        return user_id
 
 
 class Category(Base):
